a9994bb114704a01b9409b9ae71037038f3f2b15
Abiodun Oyebanji <stepheng323gmail.com>
2023-08-26 15:19:27 +0100

    add render yml file

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -8,6 +8,11 @@ const {
   SALT_ROUND,
   TOKEN_SECRET_KEY,
   TOKEN_EXPIRATION,
+  DB_USER,
+  DB_HOST,
+  DB_NAME,
+  DB_PORT,
+  DB_PASSWORD
 } = process.env;
 
 export {
@@ -16,5 +21,10 @@ export {
   SALT_ROUND,
   TOKEN_SECRET_KEY,
   TOKEN_EXPIRATION,
+  DB_USER,
+  DB_HOST,
+  DB_NAME,
+  DB_PORT,
+  DB_PASSWORD
 
 };
--- src/lib/pg.ts
+++ src/lib/pg.ts
@@ -1,10 +1,11 @@
 import { Pool } from 'pg'
+import { DB_USER, DB_HOST, DB_NAME, DB_PORT, DB_PASSWORD } from '../config/constants'
 
 export const pool = new Pool({
-  user: 'risevest',
-  host: 'db',
-  database: 'risevest',
-  password: 'risevest',
-  port: 5432,
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: Number(DB_PORT),
 });
 
