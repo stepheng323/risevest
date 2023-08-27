2968b4aa79a73015fe82252bab566ba33fa6575f
Abiodun Oyebanji <stepheng323gmail.com>
2023-08-27 07:55:03 +0100

    minor fixes

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -11,7 +11,7 @@ postRouter.post('/', checkAuth, validateCreatePost, createPost)
 postRouter.get('/users/:id', checkAuth, getUserPosts)
 
 postRouter.post('/:post_id/comments', checkAuth, validatePostComment, postComment)
-postRouter.get('/top-ranked-user', checkAuth, getTopRankedUser)
+postRouter.get('/top-ranked-user', getTopRankedUser)
 
 
 export default postRouter
