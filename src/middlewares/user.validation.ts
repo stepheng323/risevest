e7304bea44165528d671fc371ddb1d79a398bc4a
Abiodun Oyebanji <stepheng323gmail.com>
2023-08-27 07:46:32 +0100

    minor fixes

--- src/middlewares/user.validation.ts
+++ src/middlewares/user.validation.ts
@@ -10,7 +10,7 @@ export const validateCreateUser = async (req: Request, res: Response, next: Next
     firstname: Joi.string().required(),
     lastname: Joi.string().required(),
     password: Joi.string()
-      .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+      .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
 
     confirm_password: Joi.ref('password'),
   });
